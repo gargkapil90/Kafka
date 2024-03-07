adcea08286e46306570eae032829a093b4eb15c4
Kapil Garg <gargkapil90@gmail.com>
2024-03-07 20:50:50 +0530

    add delivery person project

---
+++ src/main/java/com/deliveryPerson/DeliveryPersonApp/config/KafkaConfiguration.java
@@ -0,0 +1,17 @@
+package com.deliveryPerson.DeliveryPersonApp.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder
+                .name(AppConstant.UPDATED_LOCATION_TOPIC)
+                .build();
+    }
+}
